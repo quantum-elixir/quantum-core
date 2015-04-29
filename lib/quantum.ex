3c3501fc8e48bc5b79f8f905546f31aeacb25750
Constantin Rack <constantin@rack.li>
2015-04-29 12:00:25 +0200

    Add more parsing tests.

--- lib/quantum.ex
+++ lib/quantum.ex
@@ -76,33 +76,38 @@ defmodule Quantum do
   defp match([e|t], v, min, max), do: Enum.any?(parse(e, min, max), &(&1 == v)) or match(t, v, min, max)
   defp match(e, v, min, max), do: match(e |> String.split(","), v, min, max)
 
-  defp parse("*/" <> _ = e, min, max) do
+  def parse([], _, _), do: []
+  def parse(e, min, max) when e |> is_list do
+    [h|t] = e
+    (parse(h, min, max) ++ parse(t, min, max)) |> :lists.usort
+  end
+  def parse("*/" <> _ = e, min, max) do
     [_,i] = e |> String.split("/")
     {x,_} = i |> Integer.parse
     Enum.reject(min..max, &(rem(&1, x) != 0))
   end
-  defp parse(e, min, max) do
+  def parse(e, min, max) do
     [r|i] = e |> String.split("/")
     [x|y] = r |> String.split("-")
     {v,_} = x |> Integer.parse
     parse(v, y, i, min, max) |> Enum.reject(&((&1 < min) or (&1 > max)))
   end
-  defp parse(v, [], [], _, _), do: [v]
-  defp parse(v, [], i, _, _) do
+  def parse(v, [], [], _, _), do: [v]
+  def parse(v, [], [i], _, _) do
     {x,_} = i |> Integer.parse
-    [rem(v,i)]
+    [rem(v,x)]
   end
-  defp parse(v, [y], [], min, max) do
+  def parse(v, [y], [], min, max) do
     {t,_} = y |> Integer.parse
     if v < t do
       Enum.to_list(v..t)
     else
-      Enum.to_list(v..max) ++ Enum.to_list(min..t)
+      Enum.to_list(min..t) ++ Enum.to_list(v..max)
     end
   end
-  defp parse(v, y, i, min, max) do
+  def parse(v, y, i, min, max) do
     {x, _} = i |> Integer.parse
-    parse(v, y, [], min, max) |> Enum.reject(&(rem(&1, x)) != 0)
+    parse(v, y, [], min, max) |> Enum.reject(&(rem(&1, x) != 0))
   end
 
 end
--- test/quantum_test.exs
+++ test/quantum_test.exs
@@ -17,4 +17,13 @@ defmodule QuantumTest do
     Quantum.execute(:"5 * * * *", fn -> IO.puts("FAIL") end, %{ d: { 2015, 12, 31 }, h: 12, m: 0, w: 1 } )
   end
   
+  test "parse" do
+    assert Quantum.parse("0/20", 0, 59) == [0]
+    assert Quantum.parse("10-15", 0, 59) == [10, 11, 12, 13, 14, 15]
+    assert Quantum.parse("55-100", 0, 59) == [55, 56, 57, 58, 59]
+    assert Quantum.parse("1,1,2,3,5,8" |> String.split(","), 0, 59) == [1, 2, 3, 5, 8]
+    assert Quantum.parse("*/20,30" |> String.split(","), 0, 59) == [0, 20, 30, 40]
+    assert Quantum.parse("55-5", 0, 59) == [0, 1, 2, 3, 4, 5, 55, 56, 57, 58, 59]
+  end
+  
 end
