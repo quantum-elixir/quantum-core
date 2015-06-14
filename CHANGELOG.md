75d8b48aff2f3ebe5a605895afa32fb70ac7335d
Constantin Rack <constantin@rack.li>
2015-06-14 10:02:27 +0200

    Problem: function order does not reflect invokation order

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,8 @@ All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
+### Changed
+- Function order in Quantum.Matcher
 
 ## [1.2.1] - 2015-06-13
 ### Added
--- lib/quantum/matcher.ex
+++ lib/quantum/matcher.ex
@@ -8,16 +8,16 @@ defmodule Quantum.Matcher do
     true
   end
 
-  def match([], _, _, _) do
+  def match(e, v, min, max) do
+    do_match(e |> String.split(","), v, min, max)
+  end
+
+  defp do_match([], _, _, _) do
     false
   end
 
-  def match([e|t], v, min, max) do
-    Enum.any?(parse(e, min, max), &(&1 == v)) or match(t, v, min, max)
-  end
-
-  def match(e, v, min, max) do
-    match(e |> String.split(","), v, min, max)
+  defp do_match([e|t], v, min, max) do
+    Enum.any?(parse(e, min, max), &(&1 == v)) or do_match(t, v, min, max)
   end
 
 end
