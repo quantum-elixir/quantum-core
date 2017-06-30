c619ee542afc49ae1e368c80fb51e7c5341763bc
Po Chen <chenpaul914@gmail.com>
2017-06-30 11:27:43 +1000

    Solution: simplify test setup and eliminate race condition

--- test/quantum/normalizer_test.exs
+++ test/quantum/normalizer_test.exs
@@ -10,22 +10,19 @@ defmodule Quantum.NormalizerTest do
     use Quantum.Scheduler, otp_app: :quantum_test
   end
 
-  defp start_scheduler(name) do
-    {:ok, _pid} = name.start_link()
+  setup_all do
+    Application.put_env(:quantum_test, Quantum.NormalizerTest.Scheduler, jobs: [])
+
+    {:ok, _pid} = Quantum.NormalizerTest.Scheduler.start_link()
+
     on_exit fn ->
       case Process.whereis(Quantum.Supervisor) do
         nil ->
           :ok
         pid ->
-          name.stop(pid)
+          Quantum.NormalizerTest.Scheduler.stop(pid)
       end
     end
-  end
-
-  setup do
-    Application.put_env(:quantum_test, Quantum.NormalizerTest.Scheduler, jobs: [])
-
-    start_scheduler(Quantum.NormalizerTest.Scheduler)
   end
 
   test "named job" do
