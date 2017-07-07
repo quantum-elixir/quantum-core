3024f903118be5b46d06d810e3007e35da3e4788
Jonatan Männchen <jonatan@maennchen.ch>
2017-07-07 13:08:40 +0000

    Address @princemaple's Question

--- lib/quantum/application.ex
+++ lib/quantum/application.ex
@@ -3,18 +3,16 @@ defmodule Quantum.Application do
 
   use Application
 
-  @dependency_application Application.get_env(:quantum, :date_library, Quantum.DateLibrary.Timex)
-    .dependency_application()
+  @deps Application.get_env(:quantum, :date_library, Quantum.DateLibrary.Timex)
 
-  case @dependency_application do
-    nil ->
-      def start(_type, _args) do
-        {:ok, self()}
-      end
-    _ ->
-      def start(_type, _args) do
-        Application.ensure_all_started(@dependency_application, :permanent)
-        {:ok, self()}
-      end
+  if @deps do
+    def start(_type, _args) do
+      Application.ensure_all_started(@deps, :permanent)
+      {:ok, self()}
+    end
+  else
+    def start(_type, _args) do
+      {:ok, self()}
+    end
   end
 end
