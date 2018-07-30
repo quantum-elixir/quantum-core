8dd7badca340e58fd71ca03718b0e9cb43d628d4
Tomasz Kowal <tomekowal@gmail.com>
2018-07-30 13:56:23 +0200

    Add helpers for querying task registry (#348)
    Thanks! 👍🏼

--- test/quantum/task_registry_test.exs
+++ test/quantum/task_registry_test.exs
@@ -5,7 +5,8 @@ defmodule Quantum.TaskRegistryTest do
 
   alias Quantum.TaskRegistry
 
-  doctest TaskRegistry, except: [mark_running: 3, mark_finished: 3]
+  doctest TaskRegistry,
+    except: [mark_running: 3, mark_finished: 3, is_running?: 2, any_running?: 1]
 
   setup do
     {:ok, registry} = start_supervised({TaskRegistry, __MODULE__})
@@ -42,6 +43,35 @@ defmodule Quantum.TaskRegistryTest do
       task = make_ref()
 
       assert :ok = TaskRegistry.mark_finished(registry, task, self())
+    end
+  end
+
+  describe "is_running?" do
+    test "not running", %{registry: registry} do
+      task = make_ref()
+      assert false == TaskRegistry.is_running?(registry, task)
+    end
+
+    test "running", %{registry: registry} do
+      task = make_ref()
+
+      TaskRegistry.mark_running(registry, task, self())
+
+      assert true == TaskRegistry.is_running?(registry, task)
+    end
+  end
+
+  describe "any_running?" do
+    test "not running", %{registry: registry} do
+      assert false == TaskRegistry.any_running?(registry)
+    end
+
+    test "running", %{registry: registry} do
+      task = make_ref()
+
+      TaskRegistry.mark_running(registry, task, self())
+
+      assert true == TaskRegistry.any_running?(registry)
     end
   end
 end
