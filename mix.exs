201b3ca71b0ee6665dd8f1fe57bf9a65c38bef66
Thanabodee Charoenpiriyakij <thanabodee.c@linecorp.com>
2021-07-20 10:05:44 +0700

    Fix telemetry not support v1.0
    The patch try to make this library support telemetry v1.0. Currently, it
    doesn't because it contraint between [0.4.3, 1.0.0). Fix by using `or`
    constraint to support both version.

--- mix.exs
+++ mix.exs
@@ -99,7 +99,7 @@ defmodule Quantum.Mixfile do
     [
       {:crontab, "~> 1.1"},
       {:gen_stage, "~> 0.14 or ~> 1.0"},
-      {:telemetry, ">= 0.4.3 and < 1.0.0"},
+      {:telemetry, "~> 0.4.3 or ~> 1.0.0"},
       {:tzdata, "~> 1.0", only: [:dev, :test]},
       {:ex_doc, ">= 0.0.0", only: [:dev, :docs], runtime: false},
       {:excoveralls, "~> 0.5", only: [:test], runtime: false},
