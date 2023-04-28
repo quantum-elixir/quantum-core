af88a25a8e467a2773d2c280f7e4895daff75658
Jonatan Männchen <jonatan@maennchen.ch>
2023-04-28 11:02:49 +0200

    Fix CI

--- config/.credo.exs
+++ config/.credo.exs
@@ -6,11 +6,16 @@
         included: ["lib/", "test/"],
         excluded: []
       },
-      checks: [
-        # For others you can also set parameters
-        {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 120},
-        {Credo.Check.Design.TagTODO, exit_status: 0}
-      ]
+      checks: %{
+        enabled: [
+          # For others you can also set parameters
+          {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 120},
+          {Credo.Check.Design.TagTODO, exit_status: 0}
+        ],
+        disabled: [
+          {Credo.Check.Warning.MissedMetadataKeyInLoggerConfig, []}
+        ]
+      }
     }
   ]
 }
